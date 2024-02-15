a68077ad56af65cfef7b0f5971e4651b935bd7e6
Ioannis <jondan97@gmail.com>
2024-02-15 12:02:19 +0200

    setup log4j

--- src/main/java/com/acme/university/Main.java
+++ src/main/java/com/acme/university/Main.java
@@ -1,8 +1,15 @@
 package com.acme.university;
 
-public class Main {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
         System.out.println("Hello world!");
+        log.info("test");
+        log.error("error");
     }
 }
