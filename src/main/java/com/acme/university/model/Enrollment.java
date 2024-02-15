a86aa5a396d2beb393278485bbda55d306015dcb
Ioannis <jondan97@gmail.com>
2024-02-15 12:49:13 +0200

    created model

---
+++ src/main/java/com/acme/university/model/Enrollment.java
@@ -0,0 +1,6 @@
+package com.acme.university.model;
+
+public class Enrollment extends BaseModel {
+    private Unit unit;
+    private int grade;
+}
---
+++ src/main/java/com/acme/university/model/Unit.java
@@ -0,0 +1,8 @@
+package com.acme.university.model;
+
+import java.util.List;
+
+public class Unit extends BaseModel {
+    private String name;
+    private String tutorName;
+}
